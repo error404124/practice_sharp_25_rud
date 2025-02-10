761e6327df9a4616a73e85396a5b2f2ecab3da4c
error404124 <n.rud@g.nsu.ru>
2025-02-10 14:18:03 +0700

    prices

--- App/Prices.cs
+++ App/Prices.cs
@@ -4,7 +4,41 @@ public static class Prices
 {
     public static string GetCurrencyAlias(int price, bool isShorNotation, bool isFirstCapital)
     {
-        //TODO напишите тут свое решение
-        throw new NotImplementedException();
+        string q;
+        var lastDigit = price % 10;
+        var lastTwoDigits = price % 100;
+
+        if (lastTwoDigits >= 11 && lastTwoDigits <= 14)
+            q = "рублей";
+
+        else
+        {
+            switch (lastDigit)
+            {
+                case 1:
+                    q = "рубль";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    q = "рубля";
+                    break;
+                default:
+                    q = "рублей";
+                    break;
+            }
+        }
+
+        if (isShorNotation)
+        {
+            q = "руб.";
+        }
+
+        if (isFirstCapital)
+        {
+            q = "Р" + q.Substring(1);
+        }
+
+        return q;
     }
 }
