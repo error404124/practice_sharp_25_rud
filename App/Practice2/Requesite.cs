e5149b4b120a8a873fced21ae0d294513b8e4fcc
error404124 <n.rud@g.nsu.ru>
2025-02-15 15:21:18 +0700

    practice2

---
+++ App/Practice2/Requesite.cs
@@ -0,0 +1,62 @@
+namespace App.Practice2;
+
+public class Requesite
+{
+    public static bool isValidInn(string inn)
+    {
+        if (inn.Length == 12)
+        {
+            int fisrt = 0;
+            int second = 0;
+
+            int[] innArrayFirst = { 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+            int[] innArraySecond = { 3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+
+            for (int i = 0; i < 10; i++)
+            {
+                fisrt += int.Parse(inn[i].ToString()) * innArrayFirst[i];
+            }
+
+            int tmpFirst = (fisrt % 11) % 10;
+
+            if (tmpFirst == int.Parse(inn[10].ToString()))
+            {
+                for (int i = 0; i < 11; ++i)
+                {
+                    second += int.Parse(inn[i].ToString()) * innArraySecond[i];
+                }
+
+                int tmpSecond = (second % 11) % 10;
+
+                if (tmpSecond == int.Parse(inn[11].ToString()))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        if (inn.Length == 10)
+        {
+            int[] innArray = { 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                sum += int.Parse(inn[i].ToString()) * innArray[i];
+            }
+
+            int tmp = (sum % 11) % 10;
+
+            if (tmp == int.Parse(inn[9].ToString()))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+}
---
+++ AppTests/Test2/RequesiteTest.cs
@@ -0,0 +1,22 @@
+using App.Practice2;
+
+namespace AppTests.Test2;
+
+public class RequesiteTest
+{
+    [TestCase("860322260733", true)]
+    [TestCase("1000052", false)]
+    [TestCase("379394558221", true)]
+    [TestCase("536904201650", true)]
+    [TestCase("9153885734", true)]
+    [TestCase("2605245659", true)]
+    [TestCase("2605245658", false)]
+    [TestCase("536904201651", false)]
+    [TestCase("379394558220", false)]
+    
+    public void TestisValidInn(string inn, bool expected)
+    {
+        var actual = Requesite.isValidInn(inn);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
