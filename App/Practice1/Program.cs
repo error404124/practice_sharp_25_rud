32e0370a6926e8ecacfbb4b3251332d20a9dad75
error404124 <n.rud@g.nsu.ru>
2025-02-15 22:15:04 +0700

    rectangle

--- App/Practice1/Program.cs
+++ App/Practice1/Program.cs
@@ -2,9 +2,35 @@
 
 public static class Program
 {
+
+    public static List<char> FindDuplicates(string text)
+    {
+        var answer = new List<char>();
+        var dict = new Dictionary<char, int>();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            dict.TryAdd(text[i], 0);
+            dict[text[i]]++;
+        }
+
+        foreach (var pair in dict)
+        {
+            if (pair.Value > 1)
+            {
+                answer.Add(pair.Key);
+            }
+        }
+        return answer;
+    }
+    
     public static void Main()
     {
-  
-        Console.WriteLine(Payments.CalculateTotalPayments(PaymentsPlan.Annuity, 7, 3, 10000));
+        var answer = FindDuplicates("aabbqwecc");
+        for (int i = 0; i < answer.Count; i++)
+        {
+            Console.WriteLine(answer[i]);
+        }
+
     }
 }
