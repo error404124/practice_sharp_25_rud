d2b275545788c4636f70c1f1806b2435ecb2809d
error404124 <n.rud@g.nsu.ru>
2025-02-24 23:07:34 +0700

    fix

--- App/Practice2/Requesite.cs
+++ App/Practice2/Requesite.cs
@@ -2,33 +2,49 @@ namespace App.Practice2;
 
 public class Requesite
 {
+    private static int ScalarDot(string inn, string numberName)
+    {
+        var number = 0;
+
+        if (inn.Length == 10)
+        {
+            int[] innArray = { 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+            for (var i = 0; i < 9; i++)
+            {
+                number += int.Parse(inn[i].ToString()) * innArray[i];
+            }
+        }
+
+        else if (numberName == "first")
+        {
+            int[] innArrayFirst = { 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+            for (var i = 0; i < 10; ++i)
+            {
+                number += int.Parse(inn[i].ToString()) * innArrayFirst[i];
+            }
+        }
+
+        else if (numberName == "second")
+        {
+            int[] innArraySecond = { 3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+            for (var i = 0; i < 11; ++i)
+            {
+                number += int.Parse(inn[i].ToString()) * innArraySecond[i];
+            }
+        }
+
+        return (number % 11) % 10;
+    }
+
     public static bool isValidInn(string inn)
     {
         if (inn.Length == 12)
         {
-            var first = 0;
-            var second = 0;
-
-            int[] innArrayFirst = { 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
-            int[] innArraySecond = { 3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
-
-            for (var i = 0; i < 10; i++)
+            var firstNumber = ScalarDot(inn, "first");
+            if (firstNumber == int.Parse(inn[10].ToString()))
             {
-                first += int.Parse(inn[i].ToString()) * innArrayFirst[i];
-            }
-
-            var tmpFirst = (first % 11) % 10;
-
-            if (tmpFirst == int.Parse(inn[10].ToString()))
-            {
-                for (var i = 0; i < 11; ++i)
-                {
-                    second += int.Parse(inn[i].ToString()) * innArraySecond[i];
-                }
-
-                var tmpSecond = second % 11 % 10;
-
-                if (tmpSecond == int.Parse(inn[11].ToString()))
+                var secondNumber = ScalarDot(inn, "second");
+                if (secondNumber == int.Parse(inn[11].ToString()))
                 {
                     return true;
                 }
@@ -39,17 +55,8 @@ public class Requesite
 
         if (inn.Length == 10)
         {
-            int[] innArray = { 2, 4, 10, 3, 5, 9, 4, 6, 8 };
-            var sum = 0;
-
-            for (int i = 0; i < 9; i++)
-            {
-                sum += int.Parse(inn[i].ToString()) * innArray[i];
-            }
-
-            var tmp = sum % 11 % 10;
-
-            if (tmp == int.Parse(inn[9].ToString()))
+            var firstNumber = ScalarDot(inn, null);
+            if (firstNumber == int.Parse(inn[9].ToString()))
             {
                 return true;
             }
