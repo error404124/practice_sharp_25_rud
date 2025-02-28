b6bb672580f5235034662f3040b18a4bebc6c100
error404124 <n.rud@g.nsu.ru>
2025-03-01 04:21:50 +0700

    nothing

--- App/Practice1/Program.cs
+++ App/Practice1/Program.cs
@@ -3,58 +3,9 @@ using App.Practice2;
 
 namespace App;
 
-public class User
-{
-    private string name;
-    private string inn;
-    private int age;
-
-
-    public string Inn
-    {
-        get { return inn; }
-        set
-        {
-            if (!Requesite.isValidInn(value))
-            {
-                throw new ArgumentException("Invalid Inn");
-            }
-
-            inn = value;
-        }
-    }
-
-    public int Age
-    {
-        get { return age; }
-        set
-        {
-            if (value < 0 || value > 100)
-            {
-                throw new ArgumentException("Invalid Age");
-            }
-
-            age = value;
-        }
-    }
-
-    public User()
-    {
-    }
-
-    public User(string name)
-    {
-        this.Inn = inn;
-        this.Age = age;
-        this.name = name;
-    }
-}
-
 public static class Program
 {
     public static void Main()
     {
-        var user = new User();
-        user.Inn = "9153885730";
     }
 }
