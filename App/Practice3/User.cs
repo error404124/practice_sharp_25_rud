1e59107137e7a3741926ffc777fbc7fa0351bd67
error404124 <n.rud@g.nsu.ru>
2025-03-21 21:48:37 +0700

    classFix

--- App/Practice3/User.cs
+++ App/Practice3/User.cs
@@ -2,23 +2,6 @@ using App.Practice2;
 
 namespace App.Practice3;
 
-public static class UserCreator
-{
-    public static User CreateUser(string login, string password, string name, string surname, string inn, string phone)
-    {
-        var passwordHash = CreateMd5(password);
-        return new User(login, passwordHash, name, surname, inn, phone);
-    }
-
-    private static string CreateMd5(string password)
-    {
-        var md5 = System.Security.Cryptography.MD5.Create();
-        var passwordBytes = System.Text.Encoding.ASCII.GetBytes(password);
-        var hash = md5.ComputeHash(passwordBytes);
-        return BitConverter.ToString(hash);
-    }
-}
-
 public class User
 {
     private readonly Guid id;
